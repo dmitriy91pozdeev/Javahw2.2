4d72283132128b483289ebea523fb38e201cf3a0
Name Surname <dmitriy91pozdeev@mail.ru>
2023-09-20 19:49:07 +0300

    Added code

--- src/main.java
+++ src/main.java
@@ -1,17 +1,17 @@
 public class main {
     public static void main(String[] args) {
 
-        int a = 100; // баланс
-        int b = 1100; // сумма пополнения
-        int c = b/100; // количество бонусов за пополнение
-        int d = a + b; // баланс после пополнения на сумму до 1000 руб.
-        int e = a + b + c; // баланс после пополнения на сумму более 1000 руб.
+        int balance = 100; // баланс
+        int replenishment = 1100; // сумма пополнения
+        int bonus = replenishment/100; // количество бонусов за пополнение
+        int no_bonus = balance + replenishment; // баланс после пополнения на сумму до 1000 руб.
+        int with_bonus = balance + replenishment + bonus; // баланс после пополнения на сумму более 1000 руб.
 
-        if (b <= 1000) {
-            System.out.println("Текущий баланс: " + d + " руб.");
+        if (replenishment <= 1000) {
+            System.out.println("Текущий баланс: " + no_bonus + " руб.");
         } else {
-            System.out.println("Текущий баланс: " + e + " руб.");
-            System.out.println("Бонус: " + c + " руб.");
+            System.out.println("Текущий баланс: " + with_bonus + " руб.");
+            System.out.println("Бонус: " + bonus + " руб.");
         }
 
     }
